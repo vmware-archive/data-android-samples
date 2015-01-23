79f5397c9f4b935b9be82920494cd0b087646b01
Devin Fallak and Joshua Winters <dfallak@pivotal.io>
2015-01-23 17:08:25 -0500

    Added new data SDK

---
+++ data-demo/src/main/java/io/pivotal/android/data/demo/Config.java
@@ -0,0 +1,42 @@
+package io.pivotal.android.data.demo;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Config {
+
+    private static Properties sProperties;
+
+    private static final class Keys {
+        private static final String SERVICE_URL = "pivotal.data.serviceUrl";
+    }
+
+    public static String getServiceUrl() {
+        final String value = getProperties().getProperty(Keys.SERVICE_URL);
+        return value;
+    }
+
+    private static Properties getProperties() {
+        if (sProperties == null) {
+            sProperties = loadProperties("assets/pivotal.properties");
+        }
+        return sProperties;
+    }
+
+    private static Properties loadProperties(final String path) {
+        try {
+            final Properties properties = new Properties();
+            properties.load(getInputStream(path));
+            return properties;
+        } catch (final Exception e) {
+            return null;
+        }
+    }
+
+    private static InputStream getInputStream(final String path) {
+        final Thread currentThread = Thread.currentThread();
+        final ClassLoader loader = currentThread.getContextClassLoader();
+        return loader.getResourceAsStream(path);
+    }
+
+}
--- data-demo/src/main/java/io/pivotal/android/data/demo/DataActivity.java
+++ data-demo/src/main/java/io/pivotal/android/data/demo/DataActivity.java
@@ -4,11 +4,14 @@
 package io.pivotal.android.data.demo;
 
 import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import io.pivotal.android.auth.Auth;
@@ -17,9 +20,20 @@ import io.pivotal.android.data.KeyValue;
 import io.pivotal.android.data.KeyValueObject;
 import io.pivotal.android.data.Response;
 
-public class DataActivity extends ActionBarActivity {
+public class DataActivity extends ActionBarActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private static final String REQUEST_CACHE = "PCFData:RequestCache";
+    private static final String REQUEST_KEY = "PCFData:Requests";
+
+    private static final String COLLECTION = "objects";
+    private static final String KEY = "key";
+
+    private TextView mServerText;
+    private TextView mCollectionText;
 
     private EditText mEditText;
+    private EditText mRequestCacheText;
+
     private KeyValueObject mObject;
 
     @Override
@@ -31,20 +45,39 @@ public class DataActivity extends ActionBarActivity {
         io.pivotal.android.data.Logger.setup(this);
         io.pivotal.android.auth.Logger.setup(this);
 
+        mServerText = (TextView) findViewById(R.id.server);
+        mServerText.setText(Config.getServiceUrl());
+
+        mCollectionText = (TextView) findViewById(R.id.collection);
+        mCollectionText.setText("Collection: " + COLLECTION + ", Key: " + KEY);
+
         mEditText = (EditText) findViewById(R.id.saved_text);
+        mRequestCacheText = (EditText) findViewById(R.id.request_cache);
 
-        mObject = KeyValueObject.create(this, "objects", "key");
+        mObject = KeyValueObject.create(this, COLLECTION, KEY);
+    }
 
-//        LoaderManager.enableDebugLogging(true);
+    @Override
+    protected void onStart() {
+        super.onStart();
 
-//        Data.registerTokenProvider(new TokenProvider() {
-//
-//            @Override
-//            public String provideToken() {
-//                return Auth.provideToken(this);
-//            }
-//
-//        });
+        final SharedPreferences preferences = getSharedPreferences(REQUEST_CACHE, Context.MODE_PRIVATE);
+        preferences.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        final SharedPreferences preferences = getSharedPreferences(REQUEST_CACHE, Context.MODE_PRIVATE);
+        preferences.unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(final SharedPreferences sharedPreferences, final String key) {
+        if (REQUEST_KEY.equals(key)) {
+            mRequestCacheText.setText(sharedPreferences.getString(key, ""));
+        }
     }
 
     public void onFetchClicked(final View view) {
@@ -68,6 +101,8 @@ public class DataActivity extends ActionBarActivity {
             public void onResponse(Response<KeyValue> response) {
                 if (response.isSuccess()) {
                     mEditText.setText(response.object.value);
+                } else {
+                    Toast.makeText(DataActivity.this, "data error: " + response.error.getMessage(), Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -94,6 +129,8 @@ public class DataActivity extends ActionBarActivity {
             public void onResponse(final Response<KeyValue> response) {
                 if (response.isSuccess()) {
                     mEditText.setText(response.object.value);
+                } else {
+                    Toast.makeText(DataActivity.this, "data error: " + response.error.getMessage(), Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -120,12 +157,10 @@ public class DataActivity extends ActionBarActivity {
             public void onResponse(final Response<KeyValue> response) {
                 if (response.isSuccess()) {
                     mEditText.setText(response.object.value);
+                } else {
+                    Toast.makeText(DataActivity.this, "data error: " + response.error.getMessage(), Toast.LENGTH_SHORT).show();
                 }
             }
         });
-    }
-
-    public void onRemoveClicked(final View view) {
-        Auth.removeAllAccounts(this);
     }
 }
