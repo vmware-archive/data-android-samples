694237196624983ce793438cb5b7daf247964479
Devin Fallak and Josh Winters <jwinters@pivotal.io>
2015-06-10 10:54:54 -0400

    [#96471830] Adds --refresh-dependencies to build.sh and changes mvn archive file extension to .aar

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+set -x
 
-./gradlew clean build
+./gradlew --refresh-dependencies clean build
 
--- scripts/deploy-mvn-archive.sh
+++ scripts/deploy-mvn-archive.sh
@@ -2,4 +2,4 @@
 set -e
 set -x
 
-mvn deploy:deploy-file -Dfile=$1 -Durl=file://`mvn help:evaluate -Dexpression=settings.localRepository | grep -v '[INFO]'` -DgroupId=$2 -DartifactId=$3 -Dversion=$4 -Dpackaging=jar
+mvn deploy:deploy-file -Dfile=$1 -Durl=file://`mvn help:evaluate -Dexpression=settings.localRepository | grep -v '[INFO]'` -DgroupId=$2 -DartifactId=$3 -Dversion=$4 -Dpackaging=aar
